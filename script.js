3da86a3359bc287daf26a148fa3b72ebaacaf6e6
schahnaz <schahnaz@mail.de>
2018-04-25 13:16:45 +0200

    finish task 1 and 2

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
+// First task
+
+document.write("<b>" + "Basic 1 | Reading Status" + "</b>" + "<br>" + "<br>");
 
 
-function readingStatusF() {
-
-  //  document.getElementById("randomnb").innerHTML = randnb;
-
-var library = [
-{ title: 'When Breath Becomes Air', author: 'Paul Kalanithi', readingStatus: true }, 
-{ title: 'The Girls', author: 'Emma Cline ', readingStatus: true }, 
-{ title: 'The Nest', author: 'Cynthia', readingStatus: false }, 
-{ title: 'The Crown', author: 'Kiera Cass', readingStatus: true }, 
-{ title: 'Behind Closed Doors', author: 'B.A. Paris', readingStatus: false }, 
-{ title: 'It Ends with Us ', author: 'Colleen Hoover', readingStatus: true }
+library = [
+    { title: "When Breath Becomes Air", 
+      author: "Paul Kalanithi",
+      readingStatus: true 
+    },
+    { title: "The Girls", 
+      author: "Emma Cline",
+      readingStatus: true 
+    },
+    { title: "The Nest", 
+      author: "Cynthia",
+      readingStatus: false 
+    },
+        { title: "The Crown", 
+      author: "Kiera Cass",
+      readingStatus: true 
+    },
+        { title: "Behind Closed Doors", 
+      author: "B.A. Paris",
+      readingStatus: false 
+    },
+        { title: "It Ends with Us", 
+      author: "Colleen Hoover",
+      readingStatus: true 
+    },
 ];
 
 
-/*
-    if (readingStatus == true) {
-        var msg = "read is true";
-        document.getElementById("message").innerHTML = msg;
+for (var i = 0; i < library.length; i++) {
+	if (library[i].readingStatus == true) {
+		document.write("Already read " + library[i].title + "<br>")
 
-    } else if (readingStatus == false) {
-        var msg = "read false";
-        document.getElementById("message").innerHTML = msg;
-    }
- */
+	}else {
+		document.write("You still need to read " + library[i].title + "<br>")
+
+	}
 }
+
+
+// Second Task
+
+document.write("<br>" + "<br>" + "<b>" + "Basic 2 | Truncate" + "</b>" + "<br>" + "<br>");
+
+
+    var str = "I am a hero from World of Warcraft"; 
+    var res = str.slice(0, 12);
+    var short = "Before: " + str + "<br>" + "After: " + res;
+    document.write(short);
+
