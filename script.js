c4131053c4412897f9672d1e5932baf25d16f970
schahnaz <schahnaz@mail.de>
2018-04-25 10:06:32 +0200

    add index.html and script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+function readingStatusF() {
+
+  //  document.getElementById("randomnb").innerHTML = randnb;
+
+var library = [
+{ title: 'When Breath Becomes Air', author: 'Paul Kalanithi', readingStatus: true }, 
+{ title: 'The Girls', author: 'Emma Cline ', readingStatus: true }, 
+{ title: 'The Nest', author: 'Cynthia', readingStatus: false }, 
+{ title: 'The Crown', author: 'Kiera Cass', readingStatus: true }, 
+{ title: 'Behind Closed Doors', author: 'B.A. Paris', readingStatus: false }, 
+{ title: 'It Ends with Us ', author: 'Colleen Hoover', readingStatus: true }
+];
+
+
+/*
+    if (readingStatus == true) {
+        var msg = "read is true";
+        document.getElementById("message").innerHTML = msg;
+
+    } else if (readingStatus == false) {
+        var msg = "read false";
+        document.getElementById("message").innerHTML = msg;
+    }
+ */
+}
